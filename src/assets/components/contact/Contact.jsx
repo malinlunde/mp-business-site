7d1f089b9d27ced93bfe4c1e739685c4ab49c199
Malin <malinlundhe@hotmail.com>
2024-01-06 16:00:26 +0100

    change css on tech

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -14,7 +14,7 @@ export const Contact = () => {
     <section className='contact'>
       <h2 className="h2contact">Let's talk</h2>
       <img src={jagImage} alt='' className='profile-image'/>
-      <p className='contact-name'>Malin Lundhe</p>
+      <p className='contact-name'>Malin Lunde</p>
       <p className='contact-number'>+46(0)734 33 97 18</p>
       <p className='contact-mail'>malinlundhe@hotmail.com</p>
       <div className='icons-wrapper'>
