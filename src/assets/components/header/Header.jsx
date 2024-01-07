9703792fbbef20a42509c7782f3085f3b0c6ef8e
Malin <malinlundhe@hotmail.com>
2024-01-07 19:12:38 +0100

    header corrected

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -10,15 +10,15 @@ export const Header = () => {
     <section className='intro'>
       <div className='introbox'>
         {/*Mobile View */}
+        <img src={jagImage} alt='' className='profile-image'/>
         <div className='profile-container'>
-          <img src={jagImage} alt='' className='profile-image'/>
+          <div className='headings'>
+            <h3 className='h3intro'>Hi, I'm Malin Lunde</h3>
+            <h1 className='h1intro'>Frontend Developer</h1>
+          </div>
+          <p className='pintro'>I'm diving headfirst into the world of tech. While I'm relatively new to this exciting tech scene, my curiosity drives me to explore backend development, and my front-end skills come from hands-on learning. In collaborative environments, I bring a mix of technical curiosity and organizational finesse. Quick to adapt and service-minded, I'm navigating the tech world with a happy coding vibe. Let's make some tech magic together!</p>
         </div>
-        <div className='headings'>
-          <h3 className='h3intro'>Hi, I'm Malin Lunde</h3>
-          <h1 className='h1intro'>Frontend Developer</h1>
-        </div>
-        <p className='pintro'>I'm diving headfirst into the world of tech. While I'm relatively new to this exciting tech scene, my curiosity drives me to explore backend development, and my front-end skills come from hands-on learning. In collaborative environments, I bring a mix of technical curiosity and organizational finesse. Quick to adapt and service-minded, I'm navigating the tech world with a happy coding vibe. Let's make some tech magic together!</p>
-        </div>
+      </div>
 
         {/* Desktop View */}
 
--- src/assets/components/tech/Tech.jsx
+++ src/assets/components/tech/Tech.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './tech.css';
 
 export const Tech = () => {
-  const techText = "HTML, CSS, Flexbox, Grids, JavaScript, ES7, JSX, React, React Hooks, Node.js, Npm, Web Accessibility, APIs, mob-programming, pair-programming, GitHub."
+  const techText = "HTML, CSS, Flexbox, Grids, JavaScript, ES7, JSX, Zustand, React, React Hooks, Node.js, Npm, Web Accessibility, APIs, mob-programming, pair-programming, GitHub, MongoDB, Postman, Render."
   
   return (
     <section className='tech'>
