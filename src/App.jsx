bedb1451f19df9f2ec2d0610b301002a4d9c1ce3
Malin <malinlundhe@hotmail.com>
2024-02-27 16:58:51 +0100

    added contact and about us pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import './App.css'
 import { Body } from '../src/assets/components/Body';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Contact from "../src/assets/components/contact/Contact";
 import AboutUs from "../src/assets/components/aboutus/AboutUs";
 
 export const App = () => {
   return (
     <Router>
-        <Route path="/contact" component={Contact} />
-        <Route path="/about" component={AboutUs} />
-        <Route path="/" component={Body} />
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/" element={<Body />} />
+        </Routes>
     </Router>
   );
 };
