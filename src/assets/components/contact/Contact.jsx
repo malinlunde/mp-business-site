2e953b7e391ebbed403eaaea1d4516a760f6a106
Malin <malinlundhe@hotmail.com>
2024-02-28 17:43:21 +0100

    styled contact

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -7,32 +7,20 @@ import { Link } from 'react-router-dom';
 export const Contact = () => {
   return (
     <section className='contact'>
-      <div>
+      <div className='contact-box'>
         <h2 className="h2contact">Kontakt</h2>
         <img src={logotyp} alt='' className='logotyp' />
         <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster. Tack för att du överväger M&P – där kvalitet och pålitlighet står i fokus!</p>
         <div className='contact-info'>
-          <p className='contact-label'>Namn:</p>
-          <p className='contact-detail'>John Doe</p>
-
-          <p className='contact-label'>Telefonnummer:</p>
-          <p className='contact-detail'>123-456-7890</p>
-
-          <p className='contact-label'>Mejladress:</p>
-          <p className='contact-detail'>john.doe@example.com</p>
-
-          {/* Additional contact information placeholders */}
-          {/* <p className='contact-label'>Adress:</p>
-          <p className='contact-detail'>123 Main St, City</p>
-
-          <p className='contact-label'>Social Media:</p>
-          <p className='contact-detail'>@john.doe</p> */}
+          <p className='contact-detail'>Kontakta kundansvarig John Doe</p>
+          <p className='contact-detail'>Telefonnummer: 123-456-7890</p>
+          <p className='contact-detail'>E-post: john.doe@example.com</p>
         </div>
-      </div>
-      <div className='button'>
+        <div className='button'>
         <Link to="/" className="home-button">
           &#8592; Hem
         </Link>
+      </div>
       </div>
     </section>
   );
