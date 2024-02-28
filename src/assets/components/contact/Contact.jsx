d1a6da0910e087eebb1cf0f7c9a6020a8837809f
Malin <malinlundhe@hotmail.com>
2024-02-28 18:21:45 +0100

    making site responsive

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -10,8 +10,8 @@ export const Contact = () => {
       <div className='contact-box'>
         <h2 className="h2contact">Kontakt</h2>
         <img src={logotyp} alt='' className='logotyp' />
-        <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster. Tack för att du överväger M&P – där kvalitet och pålitlighet står i fokus!</p>
         <div className='contact-info'>
+          <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster. Tack för att du överväger M&P – där kvalitet och pålitlighet står i fokus!</p>
           <p className='contact-detail'>Kontakta kundansvarig John Doe</p>
           <p className='contact-detail'>Telefonnummer: 123-456-7890</p>
           <p className='contact-detail'>E-post: john.doe@example.com</p>
