b5000d7630261165ab0d59917bad462348c9b362
Malin <malinlundhe@hotmail.com>
2024-05-08 22:21:40 +0200

    adding an intro section

--- src/assets/components/Body.jsx
+++ src/assets/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './body.css';
+import { Intro } from '../components/intro/Intro';
 import { ProjectPlating } from './projects/ProjectPlating';
 import { ProjectWalling} from './projects/ProjectWalling';
 import { ProjectFoundation } from './projects/ProjectFoundation';
@@ -9,6 +10,7 @@ import { Footer } from './footer/Footer';
 export const Body = () => {
   return (
     <div className="big-wrapper">
+        <Intro />
         <ProjectPlating />
         <ProjectWalling />
         <ProjectFoundation />
---
+++ src/assets/components/intro/Intro.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './intro.css';
+import introimage from '../images/introimage.jpeg';
+
+export const Intro = () => {
+    return (
+      <section className='intro'>
+        <div className='text-box'>
+         <h2>Din lokala markentreprenad i Sörmland</h2>
+         <p>Välkommen till M&P – det lilla företaget med den stora kompetensen inom mark och anläggning. Vi är specialiserade på att skapa hållbara och vackra utomhusmiljöer för både företag och privatpersoner. Från plattläggningar och murar till husgrunder och trädgårdsanläggningar, vi hanterar alla typer av projekt med precision och professionalism.</p>
+         <p>Bläddra gärna bland våra referensbilder för att se exempel på vårt arbete och låt dig inspireras till ditt nästa projekt. Men vi erbjuder inte bara konstruktion – vår service sträcker sig även till skötsel och underhåll. Behöver du hjälp med snöröjning eller sandupptagning? Vi är här för att göra din vardag enklare.</p>
+         <p>M&P arbetar både självständigt och i samarbete med andra företag för att leverera större entreprenader. Vårt fokus ligger alltid på kvalitet och kundnöjdhet, oavsett projektets storlek.</p>
+         <p>Kontakta oss idag för offertförfrågningar och låt oss tillsammans skapa något extraordinärt för din utomhusmiljö.</p>
+        </div>
+        <div className='intro-image'>
+        img src={introimage} alt='IntroImage' className='introimage'/>
+        </div>
+
+      </section>
+    );
+  };
+  
+  export default Intro;
