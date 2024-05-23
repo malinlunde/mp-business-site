bb0d430fe3cc8504a1581d52560e2397b60396ed
Malin <malinlundhe@hotmail.com>
2024-05-23 16:18:37 +0200

    styling contact

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import logotyp from '../images/LogoMP.png';
 import './contact.css';
-import { Link } from 'react-router-dom';
+
 
   
 export const Contact = () => {
   return (
     <section className='contact'>
-      <div className='contact-box'>
-        <div className='contact-info'>
-          <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster.</p>
-          <p className='contact-detail'>Kontakta kundansvarig Tommie Magnusson</p>
+      <div className='contact-info'>
+        <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster.</p>
+        <div className='details'>
+          <p className='contact-detail'>Kundansvarig: Tommie Magnusson</p>
           <p className='contact-detail'>Telefonnummer: 0707575067</p>
           <p className='contact-detail'>E-post: info.mpkab@gmail.com eller tommie.mpkab@gmail.com</p>
         </div>
