c7ab8c9a4510a0733b61dbf43a8946731e8991de
Malin <malinlundhe@hotmail.com>
2024-05-23 19:15:22 +0200

    changed content in intro

--- src/assets/components/intro/Intro.jsx
+++ src/assets/components/intro/Intro.jsx
@@ -7,14 +7,12 @@ export const Intro = () => {
       <section className='intro-container'>
        <div className='intro'>
         <div className='text-box'>
-         <div className='h2'>
+         <div className='h2-text'>
            <h2>Din lokala markentreprenad i Sörmland</h2>
          </div>
          <div className='ptext'>
-          <p>Välkommen till M&P – det lilla företaget med den stora kompetensen inom mark och anläggning. Vi är specialiserade på att skapa hållbara och vackra utomhusmiljöer för både företag och privatpersoner. Från plattläggningar och murar till husgrunder och trädgårdsanläggningar, vi hanterar alla typer av projekt med precision och professionalism.</p>
-          <p>Bläddra gärna bland våra referensbilder för att se exempel på vårt arbete och låt dig inspireras till ditt nästa projekt. Men vi erbjuder inte bara konstruktion – vår service sträcker sig även till skötsel och underhåll. Behöver du hjälp med snöröjning eller sandupptagning? Vi är här för att göra din vardag enklare.</p>
-          <p>M&P arbetar både självständigt och i samarbete med andra företag för att leverera större entreprenader. Vårt fokus ligger alltid på kvalitet och kundnöjdhet, oavsett projektets storlek.</p>
-          <p>Kontakta oss idag för offertförfrågningar och låt oss tillsammans skapa något extraordinärt för din utomhusmiljö.</p>
+          <p>Välkommen till M&P - det lilla företaget med den stora kompetensen inom mark och anläggning. Vi är specialister på plattläggningar, murar och husgrunder samt erbjuder service inom snöröjning och sandupptagning. Se våra referensbilder för inspiration! </p>
+          <p>Vi samarbetar med partners för större entreprenader och tar även emot enskilda uppdrag för företag och privatpersoner. Kontakta oss idag för offertförfrågningar och låt oss skapa en fantastisk utomhusmiljö tillsammans!</p>
          </div>
          
         </div>
