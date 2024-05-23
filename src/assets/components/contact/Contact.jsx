335ff969c2b7100a210955b63e453a09381b9b30
Malin <malinlundhe@hotmail.com>
2024-05-23 16:50:15 +0200

    change fonts

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -6,6 +6,9 @@ import './contact.css';
 export const Contact = () => {
   return (
     <section className='contact'>
+      <div className='h2'>
+        <h2>Kontakt</h2>
+      </div>
       <div className='contact-info'>
         <p>Kontakta M&P idag för att diskutera hur vi kan hjälpa dig med ditt nästa projekt. Vi finns här för att svara på dina frågor och ge detaljerad information om våra tjänster.</p>
         <div className='details'>
