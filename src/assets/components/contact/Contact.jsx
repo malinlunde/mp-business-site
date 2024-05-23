8cc6ab75d89af2fd43f6eec8e85d2f9d2ab218b8
Malin <malinlundhe@hotmail.com>
2024-05-23 16:27:41 +0200

    works on footer

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logotyp from '../images/LogoMP.png';
 import './contact.css';
 
 
--- src/assets/components/footer/Footer.jsx
+++ src/assets/components/footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import './footer.css';
+import logotyp from '../images/LogoMP.png';
+import fiveIcon from '../images/5.png';
 
 export const Footer = () => {
   
     return (
-        <div className='footer'>
-        (C) 2024 M&P Skapad av:  <a href="https://thetechadminsphere.com/"> thetechadminsphere</a>
-        </div>
+        <section className='footer'>
+            <div className='footer-content'>
+                <img src={logotyp} alt='Logo' className='logotyp' />
+                <img src={fiveIcon} alt='Five' className='fiveIcon' />
+                (C) 2024 M&P Skapad av:  <a href="https://thetechadminsphere.com/"> thetechadminsphere</a>
+            </div>
+        </section>
     );
   };
   
